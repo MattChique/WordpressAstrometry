1ef095b4917a5f5d9a1fb1b1e602395e21d64f5b
MatthiasG <matt@morganslions.de>
2020-04-19 14:19:43 +0200

    Added licence text

--- annotation/annotation.svg.php
+++ annotation/annotation.svg.php
@@ -1,4 +1,18 @@
-<?
+<?php
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 class SvgAnnotation extends Annotation
 {
     private $mime = "image/svg+xml";
@@ -39,7 +53,7 @@ class SvgAnnotation extends Annotation
                 rect.type-hd, line.type-hd, ellipse.type-hd { stroke: {$color_hd}; }                
 
                 {$annotation_css}
-                
+
             </style>
             <filter id="dropShadow" x="0" y="0" width="200%" height="200%">
                 <feOffset result="offOut" in="SourceAlpha" dx="1.5" dy="1.5"/>
